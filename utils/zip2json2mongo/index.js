7b09353794e95a52b83680bccde66f6d962d35c5
Koen van Gilst <koen@koenvangilst.nl>
2015-03-27 14:27:53 +0100

    rename database name utils

--- utils/zip2json2mongo/index.js
+++ utils/zip2json2mongo/index.js
@@ -17,7 +17,7 @@ var mongoCollection = process.argv[3];
 var SaveToMongo = require('save-to-mongo');
 
 var saveToMongo = SaveToMongo({
-  uri: 'mongodb://127.0.0.1:27017/termworld-2015-v3',
+  uri: 'mongodb://127.0.0.1:27017/termworld',
   collection: mongoCollection,
   bulk: {
     mode: 'unordered'
