3d7cbc315e17eb3db7e485d33791c7147c287d90
Koen van Gilst <koen@koenvangilst.nl>
2015-06-11 16:09:55 +0200

    centers search input field on landing page

--- public/js/search.js
+++ public/js/search.js
@@ -1,43 +1,72 @@
 angular.module('searchApp', [])
-	.controller('SearchController', function($http, $scope) {
 
-		var searchResults = this;
-		var hideDefaultText = false;
-		searchResults.terms = [{
-			"id": "IATE-1162091",
-			"subjectField": [10],
-			"langSet": [{
-				"lang": "de",
-				"termStr": "Kontrolle dem Umweltsicherheit der Produkte",
-				"termNote": "fullForm",
-				"relCode": 3
-			}, {
-				"lang": "nl",
-				"termStr": "milieucontrole op produkten",
-				"termNote": "fullForm",
-				"relCode": 3
-			}]
-		}, {
-			"id": "IATE-46475",
-			"subjectField": [52],
-			"langSet": [{
-				"lang": "de",
-				"termStr": "Umweltsicherheit",
-				"termNote": "fullForm",
-				"relCode": 3
-			}, {
-				"lang": "nl",
-				"termStr": "milieuveiligheid",
-				"termNote": "fullForm",
-				"relCode": 3
-			}]
-		}];
+.directive('autoFocus', function($timeout) {
+	return {
+		restrict: 'AC',
+		link: function(_scope, _element) {
+			$timeout(function() {
+				_element[0].focus();
+			}, 0);
+		}
+	};
+})
 
-		searchResults.doSearch = function() {
-			var searchString = '\"' + $scope.keyword + '\"';
-			$http.get('/api?termstr='+searchString).then(function(res) {
-				searchResults.terms = res.data;
+.directive('focusOn', function($timeout) {
+	return {
+		restrict: 'A',
+		link: function(_scope, _element, _attr) {
+			_scope.$watch(_attr.focusOn, function(_focusVal) {
+				$timeout(function() {
+					if (_focusVal) _element[0].focus();
+						else _element[0].blur();
+				}, 0);
 			});
-		};
+		}
+	};
+})
 
-	});
+.controller('SearchController', function($http, $scope) {
+
+	var searchResults = this;
+	var hideDefaultText = false;
+
+	// default values, example JSON from API
+	searchResults.terms = [{
+		"id": "IATE-1162091",
+		"subjectField": [10],
+		"langSet": [{
+			"lang": "de",
+			"termStr": "Kontrolle dem Umweltsicherheit der Produkte",
+			"termNote": "fullForm",
+			"relCode": 3
+		}, {
+			"lang": "nl",
+			"termStr": "milieucontrole op produkten",
+			"termNote": "fullForm",
+			"relCode": 3
+		}]
+	}, {
+		"id": "IATE-46475",
+		"subjectField": [52],
+		"langSet": [{
+			"lang": "de",
+			"termStr": "Umweltsicherheit",
+			"termNote": "fullForm",
+			"relCode": 3
+		}, {
+			"lang": "nl",
+			"termStr": "milieuveiligheid",
+			"termNote": "fullForm",
+			"relCode": 3
+		}]
+	}];
+
+	searchResults.doSearch = function() {
+		$scope.showLandingPage = false;
+		var searchString = '\"' + $scope.keyword + '\"';
+		$http.get('/api?termstr=' + searchString).then(function(res) {
+			searchResults.terms = res.data;
+		});
+	};
+
+});
