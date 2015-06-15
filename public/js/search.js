36acdd33e9c96750cadf11e47559e996329ba025
Koen van Gilst <koen@koenvangilst.nl>
2015-06-15 15:20:12 +0200

    refactoring firefox&explorer hack

--- public/js/search.js
+++ public/js/search.js
@@ -1,20 +1,28 @@
 angular.module('searchApp', [])
 
 .directive('focusOn', function($timeout) {
+
+	function moveCursorToEnd(el) {
+		// Creates a selection of zero characters at the end of the value
+		// This will result in moving cursor to the end of the value
+		// Necessary hack for browsers Firefox & Explorer
+		// Default behavior in Safari & Chrome
+		if (el.value) {
+			el.selectionStart = el.selectionEnd = el.value.length;
+		}
+	}
+
 	return {
 		restrict: 'A',
 		link: function(_scope, _element, _attr) {
 			_scope.$watch(_attr.focusOn, function(_focusVal) {
 				$timeout(function() {
+					// if the _focusVal is switched to true
 					if (_focusVal) {
+						// focus element
 						_element[0].focus();
-						// Creates a selection of zero characters at the end of the value
-						// This will result in moving cursor to the end of the value
-						// Necessary hack for browsers Firefox & Explorer
-						// Default behavior in Safari & Chrome
-						if(_element[0].value) {
-							_element[0].selectionStart = _element[0].selectionEnd = _element[0].value.length;
-						}
+						// movue cursor to end of input (for Firefox & Explorer)
+						moveCursorToEnd(_element[0]);
 					}
 				});
 			});
