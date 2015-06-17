996bd917c7323a72204b5207d33a545b8cd6a093
Koen van Gilst <koen@koenvangilst.nl>
2015-06-17 13:51:18 +0200

    adds highlight javascript highlight filter

--- public/js/search.js
+++ public/js/search.js
@@ -29,6 +29,14 @@ angular.module('searchApp', [])
 	};
 })
 
+.filter('highlight', function($sce) {
+	return function(text, phrase) {
+		if (phrase) text = text.replace(new RegExp('(' + phrase + ')', 'gi'),
+		'<span class="highlighted">$1</span>');
+		return $sce.trustAsHtml(text);
+	};
+})
+
 .controller('SearchController', function($http, $scope) {
 	var searchResults = this;
 	searchResults.terms = [];
