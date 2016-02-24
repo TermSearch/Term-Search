e41425d814806028dd346fc8a8c25d5fad1b9d05
Koen van Gilst <koen.vangilst@gmail.com>
2016-02-24 16:53:27 +0100

    removes TermEntry model

--- models/Schemas.js
+++ models/Schemas.js
@@ -2,38 +2,6 @@ var mongoose = require('mongoose');
 var	Schema = mongoose.Schema;
 var mongoosePaginate = require('mongoose-paginate');
 
-// var termEntrySchema = new Schema({
-// 	id: {
-// 		type: String, // eg. IATE....
-// 		index: true
-// 	},
-// 	subjectField: {
-// 		type: [Number],
-// 		index: true
-// 	}, // e.g. [123, 345]
-// 	note: String, // e.g. pharmaceutical product
-// 	langSet: [{
-// 		lang: {
-// 			type: String,
-// 			index: true
-// 		}, // e.g. "nl"
-// 		termStr: {
-// 			type: String,
-// 			index: true
-// 		}, // e.g. "vrijstelling"
-// 		termNote: String, // eg. fullForm,
-// 		relCode: Number
-// 	}]
-// });
-//
-// termEntrySchema.index({
-// 	'langSet.termStr': 'text'
-// }, {
-// 	default_language: 'german'
-// });
-//
-// termEntrySchema.plugin(mongoosePaginate);
-
 var dictEntrySchema = new Schema({
 	id: {
 		type: String, // eg. IATE....
@@ -49,12 +17,10 @@ var dictEntrySchema = new Schema({
 		index: true
 	},
 	nl: {
-		type: [String],
-		index: true
+		type: [String]
 	}
 });
 
 dictEntrySchema.plugin(mongoosePaginate);
 
-// module.exports.termEntrySchema = termEntrySchema;
 module.exports.dictEntrySchema = dictEntrySchema;
